296cb7147cba267db0c537ac88c0314593199a26
pgsmith2000 <pgsmith2000@gmail.com>
2019-11-02 00:37:37 -0500

    remove "date" from x-axis of first plot

--- 001_Six month indexes.R
+++ 001_Six month indexes.R
@@ -62,7 +62,7 @@ p1 <- p1 + geom_line(aes(color=Symbol), size=2) +
   scale_y_continuous(labels=scales::percent) +
   scale_color_manual(values=c('navyblue','darkgreen', 'peru')) +
   scale_x_date(breaks = pretty_breaks(6), date_labels = "%d %b") +
-  labs(title="1. Six-Month Change in Major Stock Indexes", color="") +
+  labs(title="1. Six-Month Change in Major Stock Indexes", x="", color="") +
   theme_minimal() +
   theme(legend.position = 'bottom') +
   theme(axis.text.x = element_text(angle = 45))
